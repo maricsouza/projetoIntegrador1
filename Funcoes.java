dacee6eb4dc606c088f210cf8aa0a536b59dcc76
Mariana <mariana0026souza@gmail.com>
2022-05-15 20:37:54 -0300

    + CenaDoisCompleta

--- Funcoes.java
+++ Funcoes.java
@@ -51,16 +51,14 @@ public class Funcoes {
     }
 
     public static void direcoesConf (int usuDirecao, int direcao[], int numDirecao) {
-        String ladoCerto = VERDE + "Voce foi para o lado certo." + BRANCO + " \nPara que lado voce quer ir?";
-        String ladoErrado = AMARELO + "Voce foi para o lado errado e teve que voltar desde o comeco do percurso.";
+
+        
         String opCenaDois [] = {"Esquerda", "Direita"};
         int resposta = 0;
         
+        System.out.println(numDirecao);
         if (direcaoCerta(usuDirecao, direcao, numDirecao)) {
             limparTela();
-            if ( papelDirecoes == true) 
-                System.out.println(AMARELO + "esquerda-direita-direita-esquerda-direita");
-            resposta = perguntaResposta(ladoCerto, opCenaDois);
             certo++;
 
         } else {
@@ -181,10 +179,8 @@ public class Funcoes {
             int teste = 0;
             int volta = 0;
             String textoCenaDois = "Ele abre a porta e da direto com um corredor sem qualquer iluminacao somente com a pouca luz de sua lamparina. Quando o jogador ultrapassa a porta, ela se fecha com forca nao o deixando voltar. Ele se encontra sem escolhas a nao ser continuar seguindo. Mais a frente, o jogador se depara com dois caminhos com algo escrito na parede entre eles." + VERMELHO + " Nao se perca." + BRANCO + " \nPara que lado voce quer ir?";
+            String ladoCerto = VERDE + "Voce foi para o lado certo." + BRANCO + " \nPara que lado voce quer ir?";
             String opCenaDois [] = {"Esquerda", "Direita"};
-
-
-            
 
             do {
                 volta++;
@@ -195,7 +191,7 @@ public class Funcoes {
 
                 if (volta == 1) {
                     resposta = perguntaResposta(textoCenaDois, opCenaDois);
-                        direcoesConf(resposta, direcoes, 0);
+                        direcoesConf(resposta, direcoes, teste);
                 }
                 
                 if (errado >= 1) {
@@ -204,14 +200,20 @@ public class Funcoes {
                     volta = 0;
                     errado = 0;
                     certo = 0;
+                    teste = 0;
 
-                } else if (certo >=1) {
-                    teste++;
-                    direcoesConf(resposta, direcoes, teste);
                 }
 
+                if (certo >= 1) {
+                    teste++;
+                    resposta = perguntaResposta(ladoCerto, opCenaDois);
+                    direcoesConf(resposta, direcoes, teste);
+                }
+                    
+                
+
  
-            } while (volta == 0 || volta <= 4);
+            } while (volta == 0 || volta < 4);
 
         } catch (Exception ex) {
             ex.printStackTrace();
